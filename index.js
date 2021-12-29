5615419e87efbcb4ba6802ec4e66b77fc49316d6
jezstdio <76942168+jezstdio@users.noreply.github.com>
2021-12-29 07:59:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,5 +21,20 @@ function handleChatButton() {
 	repeatInterval = setInterval(closeChatButton, 500);
 }
 
+// Select order number with keycombo (ctrl + q)
+function handleOrderNumber(e) {
+	const infoBlock = document.getElementsByClassName("doneInfoBlock")[0];
+	const orderNumber = infoBlock.getElementsByTagName("a")[0];
+  
+	if (e.ctrlKey && e.keyCode === 81) {
+		search.value = orderNumber.textContent;
+    	navigator.clipboard.writeText(search.value);
+    	orderNumber.click(); // navigate to the order page to check if it's loaded.
+  	}
+}
+
 // Call functions
 handleChatButton();
+
+// Call events
+document.onkeydown = handleOrderNumber;
