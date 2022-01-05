aad132cf6f440d2e37095a9aa0c1d8becd22f0d9
jezstdio <76942168+jezstdio@users.noreply.github.com>
2022-01-05 16:45:07 +0100

    Update close_chat.js

--- close_chat.js
+++ close_chat.js
@@ -1,4 +1,8 @@
-// Close chat button on bottom right side. 
+// Close chat button on bottom right side.
+// We all know the chat button on the right side of the page, at the bottom which automatically opens on every time. If you hate it like me, this script is for you.
+// Every time you visit or refresh the website, this script will automatically close it.
+// If the chat button won't be open automatically on the visit, the script will end itself after 2 seconds.
+
 function handleChatButton() {
     const chatButton = document.getElementById("chat-open-button");
     const maxRepeats = 8;
