4af779ff7c14729d89e90207b978dabb2beab303
jezstdio <jezstdio@protonmail.com>
2022-01-05 18:30:15 +0100

    remove empty spaces

--- auto_apply_workers_code.js
+++ auto_apply_workers_code.js
@@ -2,7 +2,6 @@
 // If you want to skip the continuous code acceptation on the website, just change workerCode variable to yours.
 // The script will insert your code into the right field and will close the popup afterwards.
 // The script will be activated on the cart page every time you visit it.
-
 function setWorkerCode() {
     const promoInput = document.getElementById("txtDiscountCode");
     const sendButton = document.getElementsByClassName("insertItemBtn")[0];
--- fix_tab_email_to_phone.js
+++ fix_tab_email_to_phone.js
@@ -1,7 +1,6 @@
 // When using tab, jump right to the phone number.
 // On the page when you have to give the customer's email address, by default you can't jump right to the phone number input.
 // This script sets the tab order, so you can focus the right input with just one tab.
-
 function setTabIndex() {
     const email = document.getElementById("userEmail");
     const phoneNumber = document.getElementById("inpTelNumber");
