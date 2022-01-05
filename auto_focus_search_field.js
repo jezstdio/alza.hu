76673ee99279f730b051b4de19b40f1fc73f785d
jezstdio <76942168+jezstdio@users.noreply.github.com>
2022-01-05 16:41:43 +0100

    Update auto_focus_search_field.js

--- auto_focus_search_field.js
+++ auto_focus_search_field.js
@@ -1,4 +1,5 @@
 // Focus automatically on seach field.
+// This script skips every page that it's URL address includes the word "Order". With this, you won't lost focus when you want to insert the costumer's email address.
 function autoFocusSearch() {
     const search = document.getElementById("edtSearch");
 
