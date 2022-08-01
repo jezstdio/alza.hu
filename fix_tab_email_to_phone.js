4fc204b230dfd01426eb7bbf6687b5faf4555287
jezstdio <76942168+jezstdio@users.noreply.github.com>
2022-08-01 08:48:16 +0200

    Update fix_tab_email_to_phone.js
    shorten the code by a bit

--- fix_tab_email_to_phone.js
+++ fix_tab_email_to_phone.js
@@ -12,9 +12,7 @@ function setTabIndex() {
 function autoFocusSearch() {
     const search = document.getElementById("edtSearch");
 
-    if (!window.location.href.includes("Order")) {
-        search.select();
-    }
+    if (!window.location.href.includes("Order")) { search.select() }
 }
 
 try { setTabIndex() } catch (e) { console.error(e) }
