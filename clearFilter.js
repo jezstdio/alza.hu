2e49af6d00f2ebd56cfb209b602794533f76617d
jezstdio <76942168+jezstdio@users.noreply.github.com>
2022-08-01 08:46:52 +0200

    Update clearFilter.js
    shorten the code by a bit

--- clearFilter.js
+++ clearFilter.js
@@ -3,11 +3,7 @@
 function gj_clearFilter(e) {
   const clearFilterButton = document.getElementById("clearFilter");
   
-	if (e.ctrlKey && e.shiftKey && e.keyCode === 75) {
-	  clearFilterButton.click();
-	}
+  if (e.ctrlKey && e.shiftKey && e.keyCode === 75) { clearFilterButton.click() }
 }
 
-document.addEventListener("keydown", e => {
-  try { gj_clearFilter(e) } catch (e) { console.log(e) }
-});
+document.addEventListener("keydown", e => { try { gj_clearFilter(e) } catch (e) { console.log(e) } });
