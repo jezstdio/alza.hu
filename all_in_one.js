84105b702975d7a0c74d3c5ca8e325383b134a1c
jezstdio <76942168+jezstdio@users.noreply.github.com>
2022-08-01 08:40:26 +0200

    Update all_in_one.js
    bigger code requires bigger space. Storage.sync has a limit of 9500 bytes, so comments must be removed

--- all_in_one.js
+++ all_in_one.js
@@ -1,7 +1,3 @@
-// Close chat button on bottom right side.
-// We all know the chat button on the right side of the page, at the bottom which automatically opens on every time. If you hate it like me, this script is for you.
-// Every time you visit or refresh the website, this script will automatically close it.
-// If the chat button won't be open automatically on the visit, the script will end itself after 2 seconds.
 function handleChatButton() {
     const chatButton = document.getElementById("chat-open-button");
     const maxRepeats = 8;
@@ -24,9 +20,6 @@ function handleChatButton() {
     repeatInterval = setInterval(closeChatButton, 500);
 }
 
-// Select order number with keycombo (ctrl + q)
-// If you are like me and can't stand the selection of numbers with the mouse on the final page of the order, this script is for you.
-// After it successfully run, the script automatically opens the order's page where you can check if the order has been processed and can be payed.
 function handleOrderNumber(e) {
     const infoBlock = document.getElementsByClassName("doneInfoBlock")[0];
     const orderNumber = infoBlock.getElementsByTagName("a")[0];
@@ -34,13 +27,10 @@ function handleOrderNumber(e) {
     if (e.ctrlKey && e.keyCode === 81) {
         search.value = orderNumber.textContent;
         navigator.clipboard.writeText(search.value);
-        orderNumber.click(); // navigate to the order page to check if it's loaded.
+        orderNumber.click();
     }
 }
 
-// When using tab, jump right to the phone number.
-// On the page when you have to give the customer's email address, by default you can't jump right to the phone number input.
-// This script sets the tab order, so you can focus the right input with just one tab.
 function setTabIndex() {
     const email = document.getElementById("userEmail");
     const phoneNumber = document.getElementById("inpTelNumber");
@@ -49,8 +39,6 @@ function setTabIndex() {
     phoneNumber.setAttribute("tabindex", 2);
 }
 
-// Focus automatically on seach field.
-// This script skips every page that it's URL address includes the word "Order". With this, you won't lost focus when you want to insert the costumer's email address.
 function autoFocusSearch() {
     const search = document.getElementById("edtSearch");
 
@@ -59,10 +47,6 @@ function autoFocusSearch() {
     }
 }
 
-// Automatically fill promo input with worker's code.
-// If you want to skip the continuous code acceptation on the website, just change workerCode variable to yours.
-// The script will insert your code into the right field and will close the popup afterwards.
-// The script will be activated on the cart page every time you visit it.
 function setWorkerCode() {
     const promoInput = document.getElementById("txtDiscountCode");
     const sendButton = document.getElementsByClassName("insertItemBtn")[0];
@@ -81,7 +65,6 @@ function setWorkerCode() {
     setTimeout(acceptWorkerCode, 500);
 }
 
-// This script formats automatically the VAT Number during order.
 function vatNumberFormat() {
     const input = document.getElementById("vatParamDic");
 
@@ -102,14 +85,12 @@ function gj_clearFilter(e) {
     }
 }
 
-// Call functions
 try { handleChatButton() } catch (e) { console.error(e) }
 try { setTabIndex() } catch (e) { console.error(e) }
 try { autoFocusSearch() } catch (e) { console.error(e) }
 try { vatNumberFormat() } catch(e) { console.log(e) };
 //try { setWorkerCode() } catch (e) { console.error(e) }
 
-// Call events
 document.addEventListener("keydown", e => {
   try { handleOrderNumber(e) } catch (e) { console.log(e) }
   try { gj_clearFilter(e) } catch (e) { console.log(e) }
