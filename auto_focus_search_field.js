eee0a80a73adf078fbf997985b648667737099f3
jezstdio <76942168+jezstdio@users.noreply.github.com>
2022-08-01 08:46:03 +0200

    Update auto_focus_search_field.js
    shorten the code by a bit

--- auto_focus_search_field.js
+++ auto_focus_search_field.js
@@ -3,9 +3,7 @@
 function autoFocusSearch() {
     const search = document.getElementById("edtSearch");
 
-    if (!window.location.href.includes("Order")) {
-        search.select();
-    }
+    if (!window.location.href.includes("Order")) { search.select() }
 }
 
 try { autoFocusSearch() } catch (e) { console.error(e) }
