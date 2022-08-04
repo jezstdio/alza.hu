d19abfa7ea936ab617dce85bfd91caf9dbb48130
jezstdio <jezstdio@protonmail.com>
2022-08-04 09:17:10 +0200

    finally, a right minify with merging

--- auto_focus_search_field.js
+++
@@ -1,9 +0,0 @@
-// Focus automatically on seach field.
-// This script skips every page that it's URL address includes the word "Order". With this, you won't lost focus when you want to insert the costumer's email address.
-function autoFocusSearch() {
-    const search = document.getElementById("edtSearch");
-
-    if (!window.location.href.includes("Order")) { search.select() }
-}
-
-try { autoFocusSearch() } catch (e) { console.error(e) }
